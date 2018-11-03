424f2a2bd27ae2c2337c6b3f03b8b840b56e7e57
Michael Hefner <michaelphefner@gmail.com>
2018-11-03 20:38:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  // array = array.unshift(element);
-  return array.unshift(element);
+  array = array.unshift(element);
+  return array;
 }
 function addElementToEndOfArray(array, element){
   return [...array, element];
