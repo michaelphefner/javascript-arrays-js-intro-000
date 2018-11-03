35c56bdf56d021c52cab678ffe06534afef5ada2
Michael Hefner <michaelphefner@gmail.com>
2018-11-03 20:48:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,5 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   let newArray = array;
   newArray.shift();
-  return 
+  return newArray;
 }
